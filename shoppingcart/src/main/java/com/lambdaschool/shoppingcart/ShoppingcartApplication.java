050776a9f0a35d7687fcab2d4dbb9a29ba66706e
Justin Lohner <lohner4@gmail.com>
2020-06-18 21:28:49 -0600

    Wrote tests for user controller, got to 80% coverage

--- shoppingcart/src/main/java/com/lambdaschool/shoppingcart/ShoppingcartApplication.java
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/ShoppingcartApplication.java
@@ -12,28 +12,28 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class ShoppingcartApplication {
-	// public static void main(String[] args) {
-	// 	SpringApplication.run(ShoppingcartApplication.class, args);
-	// }
-
-	@Autowired
-	private static Environment env;
-
-	private static boolean stop = false;
-
-	private static void checkEnvironmentVariable(String envvar) {
-		if (System.getenv(envvar) == null) {
-			stop = true;
-		}
-	}
-
 	public static void main(String[] args) {
-		checkEnvironmentVariable("OAUTHCLIENTID");
-		checkEnvironmentVariable("OAUTHCLIENTSECRET");
-
-		if (!stop) {
-			SpringApplication.run(ShoppingcartApplication.class,
-				args);
-		}
+		SpringApplication.run(ShoppingcartApplication.class, args);
 	}
+
+	// @Autowired
+	// private static Environment env;
+	//
+	// private static boolean stop = false;
+	//
+	// private static void checkEnvironmentVariable(String envvar) {
+	// 	if (System.getenv(envvar) == null) {
+	// 		stop = true;
+	// 	}
+	// }
+	//
+	// public static void main(String[] args) {
+	// 	checkEnvironmentVariable("OAUTHCLIENTID");
+	// 	checkEnvironmentVariable("OAUTHCLIENTSECRET");
+	//
+	// 	if (!stop) {
+	// 		SpringApplication.run(ShoppingcartApplication.class,
+	// 			args);
+	// 	}
+	// }
 }
--- shoppingcart/src/test/java/com/lambdaschool/shoppingcart/controllers/UserControllerTest.java
+++ shoppingcart/src/test/java/com/lambdaschool/shoppingcart/controllers/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.lambdaschool.shoppingcart.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lambdaschool.shoppingcart.models.*;
+import com.lambdaschool.shoppingcart.services.UserAuditing;
 import com.lambdaschool.shoppingcart.services.UserService;
 import org.junit.After;
 import org.junit.Before;
@@ -9,29 +10,43 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// @RunWith(SpringRunner.class)
+// @WebMvcTest(value = UserController.class)
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(value = UserController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@WithMockUser(username = "jimbotestv1", roles = {"USER", "ADMIN"})
 public class UserControllerTest {
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@Autowired
+	private UserAuditing userAuditing;
 
 	@MockBean
 	private UserService userService;
@@ -42,26 +57,35 @@ public class UserControllerTest {
 	public void setUp() throws Exception {
 		userList = new ArrayList<>();
 
-		ArrayList<Role> roles = new ArrayList<>();
 		Role r1 = new Role("admin");
 		r1.setRoleid(1);
-		roles.add(r1);
 
 		Role r2 = new Role("user");
 		r2.setRoleid(2);
-		roles.add(r2);
 
-		ArrayList<Cart> carts = new ArrayList<>();
-		carts.add(new Cart());
-		carts.add(new Cart());
+		ArrayList<Cart> carts1 = new ArrayList<>();
+		carts1.add(new Cart());
+		carts1.add(new Cart());
 
-		ArrayList<UserRoles> userRoles = new ArrayList<>();
-		userRoles.add(new UserRoles(new User(), r1));
-		userRoles.add(new UserRoles(new User(), r2));
-		User user1 = new User("jimbotestv1", "password", "", carts, userRoles);
+		ArrayList<UserRoles> roles1 = new ArrayList<>();
+		roles1.add(new UserRoles(new User(), r1));
+		roles1.add(new UserRoles(new User(), r2));
+		User user1 = new User("jimbotestv1", "password", "", carts1, roles1);
 		user1.setUserid(100);
 
 		userList.add(user1);
+
+		ArrayList<Cart> carts2 = new ArrayList<>();
+		carts2.add(new Cart());
+		carts2.add(new Cart());
+
+		ArrayList<UserRoles> roles2 = new ArrayList<>();
+		roles2.add(new UserRoles(new User(), r1));
+		roles2.add(new UserRoles(new User(), r2));
+		User user2 = new User("jimbotestv2", "password", "", carts2, roles2);
+		user2.setUserid(200);
+
+		userList.add(user2);
 	}
 
 	@After
@@ -86,18 +110,69 @@ public class UserControllerTest {
 	}
 
 	@Test
-	public void getUserById() {
+	public void getUserById() throws Exception {
+		String apiUrl = "/users/user/2";
+		Mockito.when(userService.findUserById(2)).thenReturn(userList.get(1));
+		RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
+
+		MvcResult r = mockMvc.perform(rb).andReturn();
+		String tr = r.getResponse().getContentAsString();
+
+		ObjectMapper mapper = new ObjectMapper();
+		String er = mapper.writeValueAsString(userList.get(1));
+
+		System.out.println("Expect - " + er);
+		System.out.println("Actual - " + tr);
+		assertEquals("Rest Api Returns List", er, tr);
 	}
 
 	@Test
-	public void getUserInfo() {
+	public void getUserInfo() throws Exception {
+		String apiUrl = "/users/myinfo";
+		Mockito.when(userService.findByName(userAuditing.getCurrentAuditor().get())).thenReturn(userList.get(0));
+		RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
+
+		MvcResult r = mockMvc.perform(rb).andReturn();
+		String tr = r.getResponse().getContentAsString();
+
+		ObjectMapper mapper = new ObjectMapper();
+		String er = mapper.writeValueAsString(userList.get(0));
+
+		System.out.println("Expect - " + er);
+		System.out.println("Actual - " + tr);
+		assertEquals("Rest Api Returns List", er, tr);
 	}
 
 	@Test
-	public void addUser() {
+	public void addUser() throws Exception {
+		// String apiUrl = "/users/user";
+		// ArrayList<Cart> thisCarts = new ArrayList<>();
+		// ArrayList<UserRoles> thisRoles = new ArrayList<>();
+		// User user3 = new User("jimbotestv3", "password", "", thisCarts, thisRoles);
+		// user3.setUserid(300);
+		// System.out.println(userService.save(any(User.class)));
+		//
+		// ObjectMapper mapper = new ObjectMapper();
+		// String userString = mapper.writeValueAsString(user3);
+		// Mockito.when(userService.save(any(User.class))).thenReturn(user3);
+		//
+		// RequestBuilder rb = MockMvcRequestBuilders
+		// 	.post(apiUrl)
+		// 	.contentType(MediaType.APPLICATION_JSON)
+		// 	.accept(MediaType.APPLICATION_JSON)
+		// 	.content(userString);
+		//
+		// mockMvc.perform(rb).andExpect(status().isCreated()).andDo(MockMvcResultHandlers.print());
 	}
 
 	@Test
-	public void deleteUserById() {
+	public void deleteUserById() throws Exception {
+		String apiUrl = "/users/user/{userid}";
+		RequestBuilder rb = MockMvcRequestBuilders
+			.delete(apiUrl, 200)
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON);
+
+		mockMvc.perform(rb).andExpect((status().isOk())).andDo(MockMvcResultHandlers.print());
 	}
 }
